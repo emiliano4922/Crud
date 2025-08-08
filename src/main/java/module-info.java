e18df45e4c945e5ddadcf461b963e71cb8f6e77c
emiliano4922 <20243rd017@utez.edu.mx>
2025-08-08 12:13:36 -0600

    Repositorio

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.example.crud {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.crud to javafx.fxml;
+    exports org.example.crud;
+}
